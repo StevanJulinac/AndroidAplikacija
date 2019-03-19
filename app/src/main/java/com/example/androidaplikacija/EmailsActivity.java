1de31fc7cc99ca7dbe5b10c46f509406a37808f0
Stevan Julinac <stevanjulinac47@gmail.com>
2019-03-19 13:27:47 +0100

    strings - titles - activity

--- app/src/main/java/com/example/androidaplikacija/EmailsActivity.java
+++ app/src/main/java/com/example/androidaplikacija/EmailsActivity.java
@@ -1,14 +1,49 @@
 package com.example.androidaplikacija;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EmailsActivity extends AppCompatActivity {
+
+    private Button BtnMove1;
+    private Button BtnMove2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emails);
+
+        BtnMove1 = findViewById(R.id.btnContact);
+        BtnMove2 = findViewById(R.id.btnContacts);
+
+        BtnMove1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveToContactActivity();
+            }
+        });
+
+        BtnMove2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveToContactsActivity();
+            }
+        });
+
+    }
+
+    private void MoveToContactActivity(){
+        Intent intent = new Intent(EmailsActivity.this, ContactActivity.class);
+        startActivity(intent);
+    }
+
+    private void MoveToContactsActivity(){
+        Intent intent = new Intent(EmailsActivity.this, ContactsActivity.class);
+        startActivity(intent);
     }
 
     @Override
