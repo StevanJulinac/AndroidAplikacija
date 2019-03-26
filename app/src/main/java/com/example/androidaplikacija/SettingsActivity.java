a0af7c42a342ec0848745f3d10c231aa2bce3f20
Stevan Julinac <stevanjulinac47@gmail.com>
2019-03-26 14:33:21 +0100

    aaa

--- app/src/main/java/com/example/androidaplikacija/SettingsActivity.java
+++ app/src/main/java/com/example/androidaplikacija/SettingsActivity.java
@@ -1,7 +1,6 @@
 package com.example.androidaplikacija;
 
 import android.preference.PreferenceActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsActivity extends PreferenceActivity {
