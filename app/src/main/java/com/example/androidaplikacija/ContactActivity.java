383abcf768ac05776e2e84b93454942c65f0ebe8
Stevan Julinac <stevanjulinac47@gmail.com>
2019-04-16 20:31:05 +0200

    Termin 5 - onOptionsItemSelected

--- app/src/main/java/com/example/androidaplikacija/ContactActivity.java
+++ app/src/main/java/com/example/androidaplikacija/ContactActivity.java
@@ -1,9 +1,14 @@
 package com.example.androidaplikacija;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -16,9 +21,30 @@ public class ContactActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.email_menu, menu);
+        inflater.inflate(R.menu.contact_menu, menu);
         return true;
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.save:
+                    Toast.makeText(ContactActivity.this,
+                            "Successfully saved", Toast.LENGTH_SHORT).show();
+                    return true;
+
+            case  R.id.back:
+            Intent intent = new Intent(this, ContactsActivity.class);
+            startActivity(intent);
+
+            default:
+            return super.onOptionsItemSelected(item);
+    }}
+
 
     @Override
     protected void onStart() {
